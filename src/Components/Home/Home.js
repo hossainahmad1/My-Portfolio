79bf2d8f4331ce9818dbd7d1277a3955c02697c7
Hossain Ahmad <hossain177922@gmail.com>
2022-12-11 20:43:36 +0600

    added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
         <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
             <div className='max-w-screen-lg mx-auto flex flex-col items-center h-full justify-center px-4 md:flex-row'>
                 <div className='flex flex-col justify-center h-full'>
-                    <h2 className='text-4xl sm:text-7xl text-white font-lg'>I am a Frontend Developer</h2>
+                    <h2 className='text-3xl sm:text-7xl text-white font-sm'>I am a Frontend Developer</h2>
                     <p className='text-gray-400 py-4 max-w-md'>I am 6 months experience in the web Developer in Progeramming-hero .I love this work, and my experience is biggest hairer education </p>
                     <div>
                         <button className='group text-white px-6 py-3 w-fit my-2 flex justify-center items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
